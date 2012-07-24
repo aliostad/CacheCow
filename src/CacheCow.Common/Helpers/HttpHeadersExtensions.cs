347c672d40494ca7baaf2293e51410330a002fa4
Ali Kheyrollahi <aliostad@gmail.com>
2012-07-24 14:29:18 +0100

    Work in progress on serialization

---
+++ src/CacheCow.Common/Helpers/HttpHeadersExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace CacheCow.Common.Helpers
+{
+	public static class HttpHeadersExtensions
+	{
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="httpHeaders">Headers</param>
+		/// <param name="headers">CR-LF separated headers as specified in HTTP spec</param>
+		public static void Parse(this HttpHeaders httpHeaders, string headers)
+		{
+			if (httpHeaders == null)
+				throw new ArgumentNullException("httpHeaders");
+
+			if (headers == null)
+				throw new ArgumentNullException("headers");
+
+			string name = null, value = null;
+			foreach (var header in headers.Split(new[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries))
+			{
+				var indexOfColon = header.IndexOf(":");
+				name = header.Substring(0, indexOfColon);
+				value = header.Substring(indexOfColon + 1).Trim();
+				if(!httpHeaders.TryAddWithoutValidation(name, value))
+					throw new InvalidOperationException(string.Format("Value {0} for header {1} not acceptable.", value, name));
+			}
+
+		}
+	}
+}
--- test/CacheCow.Tests/Client/ResponseSerializationTests.cs
+++ test/CacheCow.Tests/Client/ResponseSerializationTests.cs
@@ -33,5 +33,30 @@ namespace CacheCow.Tests.Client
 			var httpResponseMessage = defaultHttpResponseMessageSerializer.Deserialize(fileStream);
 			fileStream.Close();
 		}
+
+		[Test]
+		[Ignore]
+		public void IntegrationTest_Serialize_Deserialize()
+		{
+			var httpClient = new HttpClient();
+			var httpResponseMessage = httpClient.GetAsync("http://google.com").Result;
+			var memoryStream = new MemoryStream();
+			var defaultHttpResponseMessageSerializer = new DefaultHttpResponseMessageSerializer();
+			defaultHttpResponseMessageSerializer.Serialize(httpResponseMessage, memoryStream);
+			memoryStream.Position = 0;
+			var httpResponseMessage2 = defaultHttpResponseMessageSerializer.Deserialize(memoryStream);
+			Assert.AreEqual(httpResponseMessage.StatusCode, httpResponseMessage2.StatusCode, "StatusCode");
+			Assert.AreEqual(httpResponseMessage.ReasonPhrase, httpResponseMessage2.ReasonPhrase, "ReasonPhrase");
+			Assert.AreEqual(httpResponseMessage.Version, httpResponseMessage2.Version, "Version");
+			Assert.AreEqual(httpResponseMessage.Headers.ToString(), httpResponseMessage2.Headers.ToString(), "Headers.ToString()");
+			Assert.AreEqual(httpResponseMessage.Content.ReadAsStringAsync().Result, 
+				httpResponseMessage2.Content.ReadAsStringAsync().Result, "Content");
+
+		}
+
+
+
+
+
 	}
 }
