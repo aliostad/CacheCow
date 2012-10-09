45f1f6632b7e532c8dc9813dd706a92bd68cdc2e
Ali Kheyrollahi <aliostad@gmail.com>
2012-10-09 23:28:38 +0100

    Trying to get nuget build working

--- src/CacheCow.Server.EntityTagStore.RavenDb/Properties/AssemblyInfo.cs
+++ src/CacheCow.Server.EntityTagStore.RavenDb/Properties/AssemblyInfo.cs
@@ -6,13 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CacheCow.Server.EntityTagStore.RavenDb")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CacheCow.Server.EntityTagStore.RavenDb")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyDescription("RavenDB storage for CacheCow entity tag store")]
+[assembly: AssemblyProduct("CacheCow")]
+
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -22,15 +18,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("e8bbd4d4-da97-4517-81db-43b590d8cdf0")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
