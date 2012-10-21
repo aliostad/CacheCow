5d3de266b365931954bf1547d981dfc4f5007e0c
Ali Kheyrollahi <aliostad@gmail.com>
2012-10-21 20:23:27 +0100

    adding common assemblyinfo and upp version +Nuget

--- src/Common/CommonAssemblyInfo.cs
+++ src/Common/CommonAssemblyInfo.cs
@@ -10,6 +10,6 @@ using System.Reflection;
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyConfiguration("")]
 
-[assembly: AssemblyVersion("0.2.2.0")]
-[assembly: AssemblyFileVersion("0.2.2.0")]
-[assembly: AssemblyInformationalVersion("0.2.2")]
+[assembly: AssemblyVersion("0.2.3.0")]
+[assembly: AssemblyFileVersion("0.2.3.0")]
+[assembly: AssemblyInformationalVersion("0.2.3")]
