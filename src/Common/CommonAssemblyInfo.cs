2ddaf537dfc2f5c15a3595f6c8e8e11c334e6239
Ali Kheyrollahi <aliostad@gmail.com>
2013-08-03 18:53:24 +0100

    Upping the version

--- src/Common/CommonAssemblyInfo.cs
+++ src/Common/CommonAssemblyInfo.cs
@@ -10,6 +10,6 @@ using System.Reflection;
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyConfiguration("")]
 
-[assembly: AssemblyVersion("0.4.4.0")]
-[assembly: AssemblyFileVersion("0.4.4.0")]
-[assembly: AssemblyInformationalVersion("0.4.4")]
+[assembly: AssemblyVersion("0.4.5.0")]
+[assembly: AssemblyFileVersion("0.4.5.0")]
+[assembly: AssemblyInformationalVersion("0.4.5")]
