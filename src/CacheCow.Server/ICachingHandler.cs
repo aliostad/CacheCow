24aa5def1ba91c5ffc6ea27bc4608f7c1654897b
Ali Kheyrollahi <aliostad@gmail.com>
2013-10-05 17:49:18 +0100

    Adding documentation

--- src/CacheCow.Server/ICachingHandler.cs
+++ src/CacheCow.Server/ICachingHandler.cs
@@ -3,8 +3,17 @@
     using System;
     using System.Net.Http;
 
+    /// <summary>
+    /// This contains the only method server Caching has
+    /// </summary>
     public interface ICachingHandler
     {
+        /// <summary>
+        /// Invalidates resources passed in
+        /// All related to the same method
+        /// </summary>
+        /// <param name="method"></param>
+        /// <param name="resourceUris"></param>
         void InvalidateResources(HttpMethod method, params Uri[] resourceUris);
     }
 }
