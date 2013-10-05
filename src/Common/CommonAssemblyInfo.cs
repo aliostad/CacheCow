b1c2b70c3144d0dc898c9b6f2533053df05a82cf
Ali Kheyrollahi <aliostad@gmail.com>
2013-10-05 13:26:14 +0100

    changing version to 0.4.7

--- src/Common/CommonAssemblyInfo.cs
+++ src/Common/CommonAssemblyInfo.cs
@@ -10,6 +10,6 @@ using System.Reflection;
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyConfiguration("")]
 
-[assembly: AssemblyVersion("0.4.6.0")]
-[assembly: AssemblyFileVersion("0.4.6.0")]
-[assembly: AssemblyInformationalVersion("0.4.6")]
+[assembly: AssemblyVersion("0.4.7.0")]
+[assembly: AssemblyFileVersion("0.4.7.0")]
+[assembly: AssemblyInformationalVersion("0.4.7")]
