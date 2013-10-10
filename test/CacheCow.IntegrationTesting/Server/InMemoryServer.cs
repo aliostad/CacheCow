c717e41bf790050d6639c006952b29b8c2b019db
aliostad <aliostad@gmail.com>
2013-10-10 14:23:40 +0100

    Adding sniffer

--- test/CacheCow.IntegrationTesting/Server/InMemoryServer.cs
+++ test/CacheCow.IntegrationTesting/Server/InMemoryServer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
@@ -12,7 +13,7 @@ namespace CacheCow.IntegrationTesting.Server
     {
         private HttpSelfHostServer _server;
         private HttpSelfHostConfiguration _configuration;
-
+        private CaptureMessagesDelegatingHandler _sniffer;
         public HttpSelfHostConfiguration Configuration
         {
             get { return _configuration; }
@@ -23,6 +24,8 @@ namespace CacheCow.IntegrationTesting.Server
 
             _configuration = new HttpSelfHostConfiguration(TestConstants.BaseUrl);
             WebApiConfig.Register(Configuration);
+            _sniffer = new CaptureMessagesDelegatingHandler();
+            _configuration.MessageHandlers.Insert(0, _sniffer);
             _server = new HttpSelfHostServer(Configuration);
             _server.OpenAsync().Wait(); // yeah this looks bad ... :)
         }
@@ -37,9 +40,48 @@ namespace CacheCow.IntegrationTesting.Server
             }
         }
 
+        public HttpResponseMessage LastResponse
+        {
+            get { return _sniffer.LastResponse; }
+        }
+
+        public HttpRequestMessage LastRequest
+        {
+            get { return _sniffer.LastRequest; }
+        }
+
         public void Dispose()
         {
             Stop();
         }
+
+        private class CaptureMessagesDelegatingHandler : DelegatingHandler
+        {
+
+            private HttpRequestMessage _lastRequest;
+            private HttpResponseMessage _lastResponse;
+
+            public HttpRequestMessage LastRequest
+            {
+                get { return _lastRequest; }
+            }
+
+            public HttpResponseMessage LastResponse
+            {
+                get { return _lastResponse; }
+            }
+
+            protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, System.Threading.CancellationToken cancellationToken)
+            {
+                _lastRequest = request;
+                return base.SendAsync(request, cancellationToken)
+                    .ContinueWith(t =>
+                        {
+                            _lastResponse = t.Result;
+                            return t.Result;
+                        });
+            }
+        }
+
     }
 }
