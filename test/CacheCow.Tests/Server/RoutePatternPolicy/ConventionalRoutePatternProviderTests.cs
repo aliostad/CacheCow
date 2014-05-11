5fba24809912bec93274a31c93b9a4f9aaac367f
Ali Kheyrollahi <aliostad@gmail.com>
2014-05-11 23:27:38 +0100

    fixing issue #67

--- test/CacheCow.Tests/Server/RoutePatternPolicy/ConventionalRoutePatternProviderTests.cs
+++ test/CacheCow.Tests/Server/RoutePatternPolicy/ConventionalRoutePatternProviderTests.cs
@@ -27,6 +27,7 @@ namespace CacheCow.Tests.Server.RoutePatternPolicy
         [TestCase("api/{controller}/{id}", "http://x/api/y/123", "/api/y/+")]
         [TestCase("api/{controller}/{id}", "http://x/api/y/", "/api/y/*")]
         [TestCase("api/{controller}/{id}", "http://x/api/y", "/api/y/*")]
+        [TestCase("api/{controller}/{chichak}/{id}", "http://x/api/y", "/api/y//*")]
         [TestCase("api/{topcontroller}/{topid}/{controller}/{id}", "http://x/api/y/1/x", "/api/y/1/x/*")]
         [TestCase("api/{topcontroller}/{topid}/{controller}/{id}", "http://x/api/y/1/x/aliostad", "/api/y/1/x/+")]
         [TestCase("api/{topcontroller}/{topid}/{controller}/{action}", "http://x/api/y/1/x/aliostad", "/api/y/1/x/*")]
@@ -34,7 +35,11 @@ namespace CacheCow.Tests.Server.RoutePatternPolicy
         {
             // arg
             var configuration = new HttpConfiguration();
-            configuration.Routes.MapHttpRoute("test", routeTemplate, new {id = RouteParameter.Optional});
+            configuration.Routes.MapHttpRoute("test", routeTemplate, new
+            {
+                id = RouteParameter.Optional,
+                chichak = RouteParameter.Optional
+            });
             var request = new HttpRequestMessage(HttpMethod.Get, url);
             var routePatternProvider = new ConventionalRoutePatternProvider(configuration);
             var routeData = configuration.Routes.GetRouteData(request);
@@ -49,13 +54,18 @@ namespace CacheCow.Tests.Server.RoutePatternPolicy
 
         [TestCase("api/{controller}/{id}", "http://x/api/y/123", new[] { "/api/y/*" })]
         [TestCase("api/{controller}/{id}", "http://x/api/y/", new [] {"/api"} )]
-        [TestCase("api/{controller}/{id}", "http://x/api/y", new []{"/api"})]
+        [TestCase("api/{controller}/{id}", "http://x/api/y", new[] { "/api" })]
+        [TestCase("api/{controller}/{chichak}/{id}", "http://x/api/y", new[] { "/api/y" })]
         [TestCase("api/{parentController}/{parentId}/{controller}/{id}", "http://x/api/y/123/z/12", new[] { "/api/y/123" })]
         [TestCase("api/{parentController}/{parentId}/{controller}/{id}", "http://x/api/y/123/z/", new[] { "/api/y/123" })]
         public void GetLinkedRoutePatterns(string routeTemplate, string url, string[] exptectedPatterns)
         {
             var configuration = new HttpConfiguration();
-            configuration.Routes.MapHttpRoute("test", routeTemplate, new { id = RouteParameter.Optional });
+            configuration.Routes.MapHttpRoute("test", routeTemplate, new
+            {
+                id = RouteParameter.Optional,
+                chichak = RouteParameter.Optional
+            });
             var request = new HttpRequestMessage(HttpMethod.Get, url);
             var routePatternProvider = new ConventionalRoutePatternProvider(configuration);
             var routeData = configuration.Routes.GetRouteData(request);
@@ -82,8 +92,12 @@ namespace CacheCow.Tests.Server.RoutePatternPolicy
             var configuration = new HttpConfiguration();
             configuration.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
+                routeTemplate: "api/{chichak}/{controller}/{id}",
+                defaults: new
+                {
+                    id = RouteParameter.Optional,
+                    chichak = RouteParameter.Optional
+                }
             );
             return configuration;
         }
