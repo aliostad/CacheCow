d8c31f22c04b122542e3937cac293e301ed70ef4
damienbod <damien_bod@hotmail.com>
2014-05-19 14:01:55 +0200

    Elastic search store

---
+++ src/CacheCow.Server.EntityTagStore.Elasticsearch/PersistentCacheKey.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CacheCow.Server.EntityTagStore.Elasticsearch
+{
+    public class PersistentCacheKey
+	{
+        public const string SearchIndex = "cachecow";
+
+		public string Id { get; set; }
+
+        public string RoutePattern { get; set; }
+        
+        public string ResourceUri { get; set; }
+
+		public string ETag { get; set; }
+
+		public DateTimeOffset LastModified { get; set; }
+	}
+}
