fee983c359853e160d9e2c0afcf338b4f2dc07de
Ali Kheyrollahi <aliostad@gmail.com>
2014-08-27 23:14:42 +0100

    version 0.5.4

--- src/Common/CommonAssemblyInfo.cs
+++ src/Common/CommonAssemblyInfo.cs
@@ -10,6 +10,6 @@ using System.Reflection;
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyConfiguration("")]
 
-[assembly: AssemblyVersion("0.5.3.0")]
-[assembly: AssemblyFileVersion("0.5.3.0")]
-[assembly: AssemblyInformationalVersion("0.5.3")]
+[assembly: AssemblyVersion("0.5.4.0")]
+[assembly: AssemblyFileVersion("0.5.4.0")]
+[assembly: AssemblyInformationalVersion("0.5.4")]
