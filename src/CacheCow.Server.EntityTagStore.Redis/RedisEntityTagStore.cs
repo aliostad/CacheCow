875ed957d314df139ae8bf45b3af69bc7505c0f2
Ali Kheyrollahi <aliostad@gmail.com>
2014-09-23 00:43:27 +0100

    impl cachecow.server redis - alpha

---
+++ src/CacheCow.Server.EntityTagStore.Redis/RedisEntityTagStore.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CacheCow.Common;
+using StackExchange.Redis;
+
+namespace CacheCow.Server.EntityTagStore.Redis
+{
+    public class RedisEntityTagStore : IEntityTagStore
+    {
+        private ConnectionMultiplexer _connection;
+        private IDatabase _database;
+
+        public RedisEntityTagStore(string connectionString)
+        {
+            Init(ConnectionMultiplexer.Connect(connectionString));
+        }
+
+        public RedisEntityTagStore(ConnectionMultiplexer connection)
+        {
+            Init(connection);
+        }
+
+        public RedisEntityTagStore(IDatabase database)
+        {
+            _database = database;
+        }
+
+        private void Init(ConnectionMultiplexer connection)
+        {
+            _connection = connection;
+            _database = _connection.GetDatabase();
+        }
+
+        public void Dispose()
+        {
+            if(_connection!=null)
+                _connection.Dispose();
+        }
+
+        public TimeSpan? Expiry { get; set; }
+
+        public bool TryGetValue(CacheKey key, out TimedEntityTagHeaderValue eTag)
+        {
+            eTag = null;
+            string value = _database.StringGet(key.HashBase64);
+            if (value != null)
+            {
+                return TimedEntityTagHeaderValue.TryParse(value, out eTag);
+            }
+            return false;
+        }
+
+        public void AddOrUpdate(CacheKey key, TimedEntityTagHeaderValue eTag)
+        {
+            _database.StringSet(key.HashBase64, eTag.ToString(), Expiry);
+            _database.SetAdd("ResourceUri_" + key.ResourceUri, key.HashBase64);
+            _database.SetAdd("RoutePattern_" + key.RoutePattern, key.HashBase64);
+        }
+
+        public int RemoveResource(string resourceUri)
+        {
+            int i = 0;
+            string key = "ResourceUri_" + resourceUri;
+            var redisValues = _database.SetScan(key);
+            foreach (var value in redisValues)
+            {
+                TryRemove(value);
+                i++;
+            }
+            TryRemove(key);
+            return i;
+        }
+
+        public bool TryRemove(CacheKey key)
+        {
+            return TryRemove(key.HashBase64);
+        }
+
+        private bool TryRemove(string key)
+        {
+            return _database.KeyDelete(key);
+        }
+
+
+        public int RemoveAllByRoutePattern(string routePattern)
+        {
+            int i = 0;
+            string key = "RoutePattern_" + routePattern;
+            var redisValues = _database.SetScan(key);
+            foreach (var value in redisValues)
+            {
+                i++;
+                TryRemove(value);
+            }
+            TryRemove(key);
+            return i;
+        }
+
+        public void Clear()
+        {
+            throw new NotSupportedException("Redis does not supprt Clear().");
+        }
+    }
+}
---
+++ test/CacheCow.Server.EntityTagStore.Redis.Tests/IntegrationTests.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CacheCow.Common;
+using NUnit.Framework;
+
+namespace CacheCow.Server.EntityTagStore.Redis.Tests
+{
+    /// <summary>
+    /// Assuming local server running on default port
+    /// </summary>
+    /// 
+    [TestFixture]
+    public class IntegrationTests
+    {
+        private RedisEntityTagStore _entityTagStore;
+
+        [TearDown]
+        public void TearDown()
+        {
+            _entityTagStore.RemoveResource(GetCacheKey().ResourceUri);
+            _entityTagStore.RemoveAllByRoutePattern(GetCacheKey().RoutePattern);
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            _entityTagStore = new RedisEntityTagStore("localhost");
+        }
+
+        [Ignore]
+        [Test]
+        public void CheckStorage()
+        {
+            var cacheKey = GetCacheKey();
+            var original = GetETag();
+            _entityTagStore.AddOrUpdate(cacheKey, original);
+
+            TimedEntityTagHeaderValue etag = null;
+            Assert.IsTrue(_entityTagStore.TryGetValue(cacheKey, out etag), "retrieving failed!!");
+
+            Assert.AreEqual(original.Tag, etag.Tag);
+
+        }
+
+        [Ignore]
+        [Test]
+        public void RemoveByRoutePatternTest()
+        {
+            var cacheKey = GetCacheKey();
+            var original = GetETag();
+            _entityTagStore.AddOrUpdate(cacheKey, original);
+
+
+            var removeAllByRoutePattern = _entityTagStore.RemoveAllByRoutePattern(cacheKey.RoutePattern);
+            TimedEntityTagHeaderValue etag = null;
+            Assert.IsFalse(_entityTagStore.TryGetValue(cacheKey, out etag), "retrieving failed!!");
+
+            Assert.IsNull(etag);
+
+        }
+
+        [Ignore]
+        [Test]
+        public void RemoveByResourceUriTest()
+        {
+            var cacheKey = GetCacheKey(true);
+            var cacheKey2 = GetCacheKey(true);
+            var original = GetETag();
+            _entityTagStore.AddOrUpdate(cacheKey, original);
+            _entityTagStore.AddOrUpdate(cacheKey2, original);
+
+            var result = _entityTagStore.RemoveResource(cacheKey.ResourceUri);
+            Assert.AreEqual(2, result);
+
+        }
+
+        private CacheKey GetCacheKey(bool differentRoutPattern = false)
+        {
+            const string url = "/api/1/2/3";
+            return new CacheKey(url, new string[] { Guid.NewGuid().ToString() },
+                differentRoutPattern ?
+                url + Guid.NewGuid().ToString() :
+                url
+            );
+        }
+
+        private TimedEntityTagHeaderValue GetETag()
+        {
+            return new TimedEntityTagHeaderValue("\"" + Guid.NewGuid().ToString("N") + "\"") { LastModified = DateTime.Now };
+        }
+    }
+
+       
+}
