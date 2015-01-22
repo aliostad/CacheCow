d2600e9bda0415b717db55ce36ee8d303fdb177f
Ali Kheyrollahi <aliostad@gmail.com>
2015-01-22 21:46:15 +0000

    version 0.6.1

--- src/Common/CommonAssemblyInfo.cs
+++ src/Common/CommonAssemblyInfo.cs
@@ -12,4 +12,4 @@ using System.Reflection;
 
 [assembly: AssemblyVersion("0.6.1.0")]
 [assembly: AssemblyFileVersion("0.6.1.0")]
-[assembly: AssemblyInformationalVersion("0.1.0")]
+[assembly: AssemblyInformationalVersion("0.6.1")]
