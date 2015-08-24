b9b02af01b4cce35007d29d0e5a5f91934bc2c5c
Ali Kheyrollahi <aliostad@gmail.com>
2015-08-24 17:17:31 +0100

    changes to include symbols and push

--- scripts/publish.ps1
+++ scripts/publish.ps1
@@ -15,11 +15,10 @@ require-param $nugetApiKey -paramName "nugetApiKey"
 #safely find the solutionDir
 $ps1Dir = (Split-Path -parent $MyInvocation.MyCommand.Definition)
 $solutionDir = Split-Path -Path $ps1Dir -Parent
-$nugetExePath = resolve-path(join-path $solutionDir ".nuget")
 
 $packages = dir "$solutionDir\artifacts\packages\CacheCow.*.nupkg"
 
 foreach($package in $packages) { 
     #$package is type of System.IO.FileInfo
-    & "$nugetExePath\Nuget.exe" push $package.FullName $nugetApiKey
+    & ".\Nuget.exe" push $package.FullName $nugetApiKey
 }
