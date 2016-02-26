af0e5cdf4de79cc38f1d23e94fe6587a79739ef8
Ali Kheyrollahi <aliostad@gmail.com>
2016-02-26 15:02:10 +0000

    removing cahcecow header if already there

--- src/CacheCow.Client/Internal/HttpResponseMessageExtensions.cs
+++ src/CacheCow.Client/Internal/HttpResponseMessageExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
@@ -12,10 +13,15 @@ namespace CacheCow.Client.Internal
 		public static HttpResponseMessage AddCacheCowHeader(this HttpResponseMessage response,
 			CacheCowHeader header)
 		{
+		    var previousCacheCowHeader = response.Headers.GetCacheCowHeader();
+		    if (previousCacheCowHeader != null)
+		    {
+		        TraceWriter.WriteLine("WARNING: Already had this header: {0} NOw setting this: {1}" ,TraceLevel.Warning, previousCacheCowHeader, header);
+		        response.Headers.Remove(CacheCowHeader.Name);
+		    }
+
 			response.Headers.Add(CacheCowHeader.Name, header.ToString());
 			return response;
 		}
-
-
 	}
 }
