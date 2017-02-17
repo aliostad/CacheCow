f3f501cc568c0b10f0a9a1ffa5a249473be03ffc
Ali Kheyrollahi <aliostad@gmail.com>
2017-02-17 08:31:28 +0000

    whitespace reformat

--- src/CacheCow.Client/CacheItemMetadata.cs
+++ src/CacheCow.Client/CacheItemMetadata.cs
@@ -5,11 +5,11 @@ using System.Text;
 
 namespace CacheCow.Client
 {
-	public class CacheItemMetadata
-	{
-		public Byte[] Key { get; set; }
-		public DateTime LastAccessed { get; set; }
-		public long Size { get; set; }
-		public string Domain { get; set; }
-	}
+    public class CacheItemMetadata
+    {
+        public Byte[] Key { get; set; }
+        public DateTime LastAccessed { get; set; }
+        public long Size { get; set; }
+        public string Domain { get; set; }
+    }
 }
