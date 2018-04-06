0f0303968d0e9d1c130e2a6e65f5a03b0db94f33
Ali Kheyrollahi <aliostad@gmail.com>
2018-04-06 12:34:38 +0100

    restructure etag extraction

---
+++ src/CacheCow.Server.Core/Directives/CacheDirectiveProviderBase.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+
+namespace CacheCow.Server.Core
+{
+    public abstract class CacheDirectiveProviderBase : ICacheDirectiveProvider
+    {
+        private readonly ITimedETagExtractor _timedETagExtractor;
+
+        public CacheDirectiveProviderBase(ITimedETagExtractor timedETagExtractor)
+        {
+            _timedETagExtractor = timedETagExtractor;
+        }
+
+        /// <summary>
+        /// Whether it is worth trying to extracting TETHV
+        /// If resource is non-cacheable it is not worth it
+        /// </summary>
+        /// <returns></returns>
+        protected abstract bool ShouldTryExtract();
+
+        public TimedEntityTagHeaderValue Extract(object viewModel)
+        {
+            return ShouldTryExtract() ? _timedETagExtractor.Extract(viewModel) : null;
+        }
+
+        public abstract CacheControlHeaderValue Get(HttpContext context);
+        
+    }
+
+    public abstract class CacheDirectiveProviderBase<TViewModel> : CacheDirectiveProviderBase, ICacheDirectiveProvider<TViewModel>
+    {
+        public CacheDirectiveProviderBase(ITimedETagExtractor timedETagExtractor) : base(timedETagExtractor)
+        {
+        }
+
+        public TimedEntityTagHeaderValue Extract(TViewModel t)
+        {
+            return base.Extract(t);
+        }
+    }
+
+}
---
+++ test/CacheCow.Server.Core.Mvc.Tests/WithQueryProviderTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace CacheCow.Server.Core.Mvc.Tests
+{
+    public class WithQueryProviderTests
+    {
+        private TestServer _server;
+        private HttpClient _client;
+
+        public WithQueryProviderTests()
+        {
+            _server = new TestServer(new WebHostBuilder()
+                .UseStartup<WithQueryProviderStartup>());
+            _client = _server.CreateClient();
+        }
+
+    }
+
+    public class WithQueryProviderStartup
+    {
+        public WithQueryProviderStartup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public virtual void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddHttpCaching();
+        }
+
+        public virtual void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "api-getall",
+                    defaults: new { action = "GETALL" },
+                    template: "api/{controller}/");
+
+                routes.MapRoute(
+                    name: "api-get",
+                    defaults: new { action = "GET" },
+                    template: "api/{controller}/{id:int}");
+            });
+
+        }
+
+    }
+}
