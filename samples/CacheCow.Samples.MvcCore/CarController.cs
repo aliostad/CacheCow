7be12bf2d2ac34d4447674e320de7f54409b5588
Ali Kheyrollahi <aliostad@gmail.com>
2018-04-08 09:03:06 +0100

    adding mvc core samples project

---
+++ samples/CacheCow.Samples.MvcCore/CarController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CacheCow.Samples.MvcCore
+{
+    public class CarController
+    {
+
+    }
+}
---
+++ samples/CacheCow.Samples.MvcCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CacheCow.Samples.MvcCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
