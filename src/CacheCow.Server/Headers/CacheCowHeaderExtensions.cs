53a08b74e9d2fd668bd0e61e18ae63c0045830a5
Ali Kheyrollahi <aliostad@gmail.com>
2018-04-10 18:33:26 +0100

    missing!

---
+++ src/CacheCow.Server/Headers/CacheCowHeaderExtensions.cs
@@ -0,0 +1,51 @@
+﻿#if NET452
+#else
+using Microsoft.AspNetCore.Http;
+#endif
+using System.Net.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CacheCow.Server.Headers
+{
+    public static class CacheCowHeaderExtensions
+    {
+        /// <summary>
+        /// Extracts CacheCowHeader (server) if one exists
+        /// </summary>
+        /// <param name="response"></param>
+        /// <returns></returns>
+        public static CacheCowHeader GetCacheCowHeader(
+            this HttpResponseMessage response)
+        {
+            CacheCowHeader header = null;
+            if (response.Headers.Contains(CacheCowHeader.Name))
+            {
+                CacheCowHeader.TryParse(response.Headers.GetValues(CacheCowHeader.Name).FirstOrDefault(), out header);
+            }
+
+            return header;
+        }
+
+#if NET452
+#else
+        /// <summary>
+        /// Extracts CacheCowHeader (server) if one exists
+        /// </summary>
+        /// <param name="response"></param>
+        /// <returns></returns>
+        public static CacheCowHeader GetCacheCowHeader(
+            this HttpResponse response)
+        {
+            CacheCowHeader header = null;
+            if (response.Headers.ContainsKey(CacheCowHeader.Name))
+            {
+                CacheCowHeader.TryParse(response.Headers[CacheCowHeader.Name], out header);
+            }
+
+            return header;
+        }
+#endif
+    }
+}
