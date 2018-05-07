23f9e29fba2ccdec5d271be1a06b5f0f2221d095
AliKheyrollahi <aliostad+github@gmail.com>
2018-05-08 00:41:37 +0100

    fixing x-plat build

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-dotnet restore -r netcoreapp2.0 -r netstandard2.0
-dotnet test -f netcoreapp2.0
+dotnet restore CacheCow.sln -r netcoreapp2.0 -r netstandard2.0
+dotnet test CacheCow.sln -f netcoreapp2.0
