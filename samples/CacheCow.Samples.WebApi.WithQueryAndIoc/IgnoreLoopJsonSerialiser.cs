b9630730a25468f37423f4d53eea57ba62348958
Ali Kheyrollahi <aliostad@gmail.com>
2018-12-27 13:36:52 +0000

    example of how replace a dependency

---
+++ samples/CacheCow.Samples.WebApi.WithQueryAndIoc/IgnoreLoopJsonSerialiser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CacheCow.Server;
+using Newtonsoft.Json;
+
+namespace CacheCow.Samples.WebApi.WithQueryAndIoc
+{
+    class IgnoreLoopJsonSerialiser : ISerialiser
+    {
+        public byte[] Serialise(object o)
+        {
+            Console.WriteLine("Serialising by IgnoreLoopJsonSerialiser");
+
+            JsonSerializerSettings settings = new JsonSerializerSettings()
+            {
+                ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore,
+                PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.Objects
+            };
+
+            return Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(o, settings));
+        }
+    }
+}
