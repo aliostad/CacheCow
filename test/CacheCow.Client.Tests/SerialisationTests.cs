fae534c78958644a02ae558ea36e7cbef4e5a104
Ali Kheyrollahi <aliostad@gmail.com>
2019-10-19 17:24:28 +0100

    fixing 2 flickering tests

--- test/CacheCow.Client.Tests/SerialisationTests.cs
+++ test/CacheCow.Client.Tests/SerialisationTests.cs
@@ -20,18 +20,21 @@ namespace CacheCow.Client.Tests
 		[Fact]
 		public async Task Response_Deserialize_Serialize()
 		{
-            var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("CacheCow.Client.Tests.Data.Response.cs");
-			var serializer = new MessageContentHttpMessageSerializer();
-			var response = await serializer.DeserializeToResponseAsync(stream);
+            using (var stream = new FileStream("Data/Response.cs", FileMode.Open))
+            {
+                var serializer = new MessageContentHttpMessageSerializer();
+                var response = await serializer.DeserializeToResponseAsync(stream);
 
-			var memoryStream = new MemoryStream();
-			await serializer.SerializeAsync(response, memoryStream);
+                var memoryStream = new MemoryStream();
+                await serializer.SerializeAsync(response, memoryStream);
 
-			memoryStream.Position = 0;
-			var response2 = await serializer.DeserializeToResponseAsync(memoryStream);
-			var result = DeepComparer.Compare(response, response2);
-			if(result.Count()>0)
-				throw new Exception(string.Join("\r\n", result));
+                memoryStream.Position = 0;
+                var response2 = await serializer.DeserializeToResponseAsync(memoryStream);
+                var result = DeepComparer.Compare(response, response2);
+                if(result.Count()>0)
+                    throw new Exception(string.Join("\r\n", result));
+
+            }
 		}
 
 		[Fact]
@@ -53,21 +56,23 @@ namespace CacheCow.Client.Tests
 		[Fact]
 		public async Task Response_Deserialize_Serialize_File()
 		{
-            var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("CacheCow.Client.Tests.Data.Response.cs");
-			var serializer = new MessageContentHttpMessageSerializer();
-			var response = await serializer.DeserializeToResponseAsync(stream);
+            using (var stream = new FileStream("Data/Response.cs", FileMode.Open))
+            {
+                var serializer = new MessageContentHttpMessageSerializer();
+                var response = await serializer.DeserializeToResponseAsync(stream);
 
-			using(var fileStream = new FileStream(Path.GetTempFileName(), FileMode.Create))
-			{
-				await serializer.SerializeAsync(response, fileStream);
+                using(var fileStream = new FileStream(Path.GetTempFileName(), FileMode.Create))
+                {
+                    await serializer.SerializeAsync(response, fileStream);
 
-				fileStream.Position = 0;
-				var response2 = await serializer.DeserializeToResponseAsync(fileStream);
-				var result = DeepComparer.Compare(response, response2);
-				if (result.Count() > 0)
-					throw new Exception(string.Join("\r\n", result));
-			}
-		}
+                    fileStream.Position = 0;
+                    var response2 = await serializer.DeserializeToResponseAsync(fileStream);
+                    var result = DeepComparer.Compare(response, response2);
+                    if (result.Count() > 0)
+                        throw new Exception(string.Join("\r\n", result));
+                }
+            }
+        }
 
 		[Fact]
 		public async Task Request_Deserialize_Serialize_File()
