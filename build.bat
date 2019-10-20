b5d17f1acbe085f27eb336e285bb92cef6c73cf8
Ali Kheyrollahi <aliostad@gmail.com>
2019-10-20 15:59:50 +0100

    adding trx

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 dotnet build CacheCow.sln
-dotnet test .\test\CacheCow.Tests\CacheCow.Tests.csproj
-dotnet test .\test\CacheCow.Client.Tests\CacheCow.Client.Tests.csproj
-dotnet test .\test\CacheCow.Server.Core.Mvc.Tests\CacheCow.Server.Core.Mvc.Tests.csproj
-dotnet test .\test\CacheCow.Client.RedisCacheStore.Tests\CacheCow.Client.RedisCacheStore.Tests.csproj
+dotnet test .\test\CacheCow.Tests\CacheCow.Tests.csproj -l trx
+dotnet test .\test\CacheCow.Client.Tests\CacheCow.Client.Tests.csproj -l trx
+dotnet test .\test\CacheCow.Server.Core.Mvc.Tests\CacheCow.Server.Core.Mvc.Tests.csproj -l trx
+dotnet test .\test\CacheCow.Client.RedisCacheStore.Tests\CacheCow.Client.RedisCacheStore.Tests.csproj -l trx
