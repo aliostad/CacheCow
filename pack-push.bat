fd82ed111b8b19481510e5ba5b5a03accc1a3b14
Ali Kheyrollahi <aliostad@gmail.com>
2019-10-27 21:50:47 +0000

    conditional push

--- pack-push.bat
+++ pack-push.bat
@@ -1,3 +1,7 @@
 del /F /Q .\artifacts\*.*
-dotnet pack CacheCow.sln -o .\artifacts
-dotnet nuget push "artifacts\*.nupkg" -s nuget.org
+dotnet pack CacheCow.sln -o .\artifacts -c Release
+
+IF NOT DEFINED [%api_key%]
+	dotnet nuget push "artifacts\*.nupkg" -s nuget.org
+ELSE
+	dotnet nuget push "artifacts\*.nupkg" -s nuget.org -k $(api_key)
