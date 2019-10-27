f7ad9e04e0d815bcd85dfdcbbe0347352a0904e6
Ali Kheyrollahi <aliostad@gmail.com>
2019-10-27 22:35:56 +0000

    ta se nashe

--- pack-push.bat
+++ pack-push.bat
@@ -1,6 +1,6 @@
 del /F /Q .\artifacts\*.*
 dotnet pack CacheCow.sln -o .\artifacts -c Release
 if "%1" == "" (
-	dotnet nuget push "artifacts\*.nupkg" -s nuget.org )
+	dotnet nuget push "artifacts\*.nupkg" -n -s nuget.org )
 if NOT "%1" == ""  (
-	dotnet nuget push "artifacts\*.nupkg" -s nuget.org -k "%1" )
+	dotnet nuget push "artifacts\*.nupkg" -n -s nuget.org -k "%1" )
