068f735b4cae4e3186081bbb37a97bc68f905ab7
Ali Kheyrollahi <ali.kheyrollahi@dominos.co.uk>
2019-10-28 11:32:06 +0000

    nuget is bane

--- pack-push.bat
+++ pack-push.bat
@@ -1,9 +1,7 @@
 del /F /Q .\artifacts\*.*
 dotnet pack CacheCow.sln -o .\artifacts -c Release
 if "%1" == "" (
-	echo dotnet nuget push "artifacts\*.nupkg" -s nuget.org --no-symbols
 	dotnet nuget push "artifacts\*.nupkg" -s nuget.org --no-symbols )
 if NOT "%1" == ""  (
-	echo dotnet nuget push "artifacts\*.nupkg" -s nuget.org --no-symbols --api-key "%1"
-	dotnet nuget push "artifacts\*.nupkg" -s nuget.org --no-symbols --api-key "%1" 
+	dotnet nuget push "artifacts\*.nupkg" -s nuget.org --api-key "%1" --no-symbols
 	)
