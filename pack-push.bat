0a757e887482af29f8a9b1d523fe2c77a48f6bbe
Ali Kheyrollahi <ali.kheyrollahi@dominos.co.uk>
2019-10-28 10:35:55 +0000

    shikari

--- pack-push.bat
+++ pack-push.bat
@@ -1,6 +1,9 @@
 del /F /Q .\artifacts\*.*
 dotnet pack CacheCow.sln -o .\artifacts -c Release
 if "%1" == "" (
+	echo dotnet nuget push "artifacts\*.nupkg" -s nuget.org --no-symbols
 	dotnet nuget push "artifacts\*.nupkg" -s nuget.org --no-symbols )
 if NOT "%1" == ""  (
-	dotnet nuget push "artifacts\*.nupkg" -s nuget.org --no-symbols --api-key "%1" )
+	echo dotnet nuget push "artifacts\*.nupkg" -s nuget.org --no-symbols --api-key "%1"
+	dotnet nuget push "artifacts\*.nupkg" -s nuget.org --no-symbols --api-key "%1" 
+	)
