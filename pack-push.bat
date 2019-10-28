257d34b08001118f2a3fef16ba219cbbb89a234f
Ali Kheyrollahi <ali.kheyrollahi@dominos.co.uk>
2019-10-28 09:28:57 +0000

    this time is different

--- pack-push.bat
+++ pack-push.bat
@@ -3,4 +3,4 @@ dotnet pack CacheCow.sln -o .\artifacts -c Release
 if "%1" == "" (
 	dotnet nuget push "artifacts\*.nupkg" -s nuget.org -n )
 if NOT "%1" == ""  (
-	dotnet nuget push "artifacts\*.nupkg" -s nuget.org -k "%1" )
+	dotnet nuget push "artifacts\*.nupkg" -s nuget.org -n -k "%1" )
