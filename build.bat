8dacc34aa9d7afa428c685195a410be34f7774a2
aliostad <aliostad+github@github.com>
2023-03-31 19:30:38 +0100

    Pushing NATS kv store to nuget - version 2.12.1

--- build.bat
+++ build.bat
@@ -4,3 +4,4 @@ dotnet test .\test\CacheCow.Client.Tests\CacheCow.Client.Tests.csproj -l trx
 dotnet test .\test\CacheCow.Server.Core.Mvc.Tests\CacheCow.Server.Core.Mvc.Tests.csproj -l trx
 dotnet test .\test\CacheCow.Server.WebApi.Tests\CacheCow.Server.WebApi.Tests.csproj -l trx
 dotnet test .\test\CacheCow.Client.RedisCacheStore.Tests\CacheCow.Client.RedisCacheStore.Tests.csproj -l trx
+dotnet test ./test/CacheCow.Client.NatsKeyValueCacheStore.Tests/CacheCow.Client.NatsKeyValueCacheStore.Tests.csproj -l trx
